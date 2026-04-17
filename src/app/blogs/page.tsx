3f7d40d6eb8247a76607c2d13b5b912e02e792ff
Jain4556 <sunil925business@gmail.com>
2026-04-17 15:47:30 +0530

    blogs add

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,10 @@
+export default function Blogs() {
+  return (
+    <div className="container py-20">
+      <h1 className="text-4xl font-bold">Blogs</h1>
+      <p className="mt-4 text-gray-400">
+        Blogs coming soon...
+      </p>
+    </div>
+  );
+}
